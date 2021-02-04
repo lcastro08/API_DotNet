24e69a9cf368326395fab6c14501bad323b8722a
lcastro <luis_9008@hotmail.com>
2021-02-03 22:18:34 -0600

    API en DotNET 5

---
+++ Api_net5/Controllers/BeerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_net5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeerController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Models.Beer> Get()
+        {
+            using (var db = new Models.beerDBContext())
+            {
+                IEnumerable<Models.Beer> beers = db.Beers.ToList();
+                return beers;
+            }
+        }
+    }
+}
---
+++ Api_net5/Models/Beer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Api_net5.Models
+{
+    public partial class Beer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
